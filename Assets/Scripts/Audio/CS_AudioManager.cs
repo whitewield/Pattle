e645f1bf7da14fd23068178a5872653a73159a7a
Hang <whitewield@126.com>
2017-03-21 12:18:16 -0400

    SFX List

--- Assets/Scripts/Audio/CS_AudioManager.cs
+++ Assets/Scripts/Audio/CS_AudioManager.cs
@@ -2,14 +2,11 @@
 //http://www.blog.silentkraken.com/2010/04/06/audiomanager/
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CS_AudioManager : MonoBehaviour {
 	
 	private static CS_AudioManager instance = null;
-
-	public GameObject myPrefabSFX;
-
-	public AudioSource myAudioSource;
 
 	//========================================================================
 	public static CS_AudioManager Instance {
@@ -29,6 +26,27 @@ public class CS_AudioManager : MonoBehaviour {
 	}
 	//========================================================================
 
+	[SerializeField] GameObject mySFXPrefab;
+	[SerializeField] int mySFXMaxNumber = 50;
+	private List<GameObject> mySFXList = new List<GameObject> ();
+
+	[SerializeField] AudioSource myAudioSource;
+
+	void Start () {
+		for (int i = 0; i < mySFXMaxNumber; i++) {
+			mySFXList.Add (Instantiate (mySFXPrefab, this.transform));
+		}
+	}
+
+	private AudioSource GetIdleAudioSource () {
+		for (int i = 0; i < mySFXList.Count; i++) {
+			if (mySFXList [i].GetComponent<AudioSource> ().isPlaying == false)
+				return mySFXList [i].GetComponent<AudioSource> ();
+		}
+		Debug.LogError ("Can not find idle audio source!");
+		return null;
+	}
+
 	public void PlaySFX (CS_AudioClip g_SFX) {
 		AudioClip t_AudioClip = g_SFX.GetMyAudioClip ();
 
@@ -37,25 +55,47 @@ public class CS_AudioManager : MonoBehaviour {
 			return;
 		}
 
-		GameObject t_SFX = Instantiate (myPrefabSFX) as GameObject;
-		t_SFX.name = "SFX_" + t_AudioClip.name;
-		t_SFX.GetComponent<AudioSource> ().clip = t_AudioClip;
-		t_SFX.GetComponent<AudioSource> ().volume = g_SFX.myVolum;
-		t_SFX.GetComponent<AudioSource> ().pitch = g_SFX.GetMyPitch ();
-		t_SFX.GetComponent<AudioSource> ().Play ();
-		DestroyObject(t_SFX, t_AudioClip.length);
+		AudioSource t_SFXAudioSource = GetIdleAudioSource ();
+		if (t_SFXAudioSource == null) {
+			Debug.LogError ("Can not find idle audio source!");
+			return;
+		}
+
+		t_SFXAudioSource.clip = t_AudioClip;
+		t_SFXAudioSource.volume = g_SFX.myVolum;
+		t_SFXAudioSource.pitch = g_SFX.GetMyPitch ();
+		t_SFXAudioSource.Play ();
+
+//		GameObject t_SFX = Instantiate (mySFXPrefab) as GameObject;
+//		t_SFX.name = "SFX_" + t_AudioClip.name;
+//		t_SFX.GetComponent<AudioSource> ().clip = t_AudioClip;
+//		t_SFX.GetComponent<AudioSource> ().volume = g_SFX.myVolum;
+//		t_SFX.GetComponent<AudioSource> ().pitch = g_SFX.GetMyPitch ();
+//		t_SFX.GetComponent<AudioSource> ().Play ();
+//		DestroyObject(t_SFX, t_AudioClip.length);
 	}
 
-	public void PlaySFX (AudioClip g_SFX) {
+	public void PlaySFX (AudioClip g_SFX, float g_volum = 1) {
 		if (g_SFX == null) {
 			Debug.LogWarning ("Can not find the sound effect!");
 			return;
 		}
-		GameObject t_SFX = Instantiate (myPrefabSFX) as GameObject;
-		t_SFX.name = "SFX_" + g_SFX.name;
-		t_SFX.GetComponent<AudioSource> ().clip = g_SFX;
-		t_SFX.GetComponent<AudioSource> ().Play ();
-		DestroyObject(t_SFX, g_SFX.length);
+
+		AudioSource t_SFXAudioSource = GetIdleAudioSource ();
+		if (t_SFXAudioSource == null) {
+			Debug.LogError ("Can not find idle audio source!");
+			return;
+		}
+
+		t_SFXAudioSource.clip = g_SFX;
+		t_SFXAudioSource.volume = g_volum;
+		t_SFXAudioSource.Play ();
+
+//		GameObject t_SFX = Instantiate (mySFXPrefab) as GameObject;
+//		t_SFX.name = "SFX_" + g_SFX.name;
+//		t_SFX.GetComponent<AudioSource> ().clip = g_SFX;
+//		t_SFX.GetComponent<AudioSource> ().Play ();
+//		DestroyObject(t_SFX, g_SFX.length);
 	}
 
 	public void PlayBGM (AudioClip g_BGM) {
