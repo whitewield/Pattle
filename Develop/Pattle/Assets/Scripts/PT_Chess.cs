754b9c792b3a37b8c3574a423c3d6a4b4f91b302
Hang <whitewield@126.com>
2017-09-12 10:42:11 -0400

    new character

---
+++ Develop/Pattle/Assets/Scripts/PT_Chess.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Pattle {
+	namespace Chess{
+		public class PT_Chess : NetworkBehaviour {
+			[SyncVar][SerializeField] PT_ChessAttributes myAttributes;
+
+			// Use this for initialization
+			void Start () {
+
+			}
+
+			// Update is called once per frame
+			void Update () {
+
+			}
+		}
+			
+		[System.Serializable]
+		public class PT_ChessAttributes {
+			//HealthPoint
+			[SerializeField] int at_HP;
+			//CoolDown
+			[SerializeField] float at_CD;
+			//CastTime
+			[SerializeField] float at_CT;
+			//damage and defence
+			[SerializeField] int at_PDM;
+			[SerializeField] int at_PDF;
+			[SerializeField] int at_MDM;
+
+			//Current HealthPoint
+			private int at_CurHP;
+		}
+	}
+}
