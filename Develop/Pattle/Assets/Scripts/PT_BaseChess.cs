39bfae27c6fda399d4df2061bb823ce6158a046e
Hang <whitewield@126.com>
2017-09-13 18:25:47 -0400

    retry network

--- Develop/Pattle/Assets/Scripts/PT_BaseChess.cs
+++ Develop/Pattle/Assets/Scripts/PT_BaseChess.cs
@@ -3,23 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class PT_ServerManager : NetworkBehaviour {
-
-	[SerializeField] GameObject enemyPrefab;
-	[SerializeField] int numberOfEnemies;
-
-	public override void OnStartServer ()
-	{
-
-	}
+public class PT_BaseChess : NetworkBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+		
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
-
+		
 	}
 }
---
+++ Develop/Pattle/Assets/Scripts/PT_PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PT_PlayerController : NetworkBehaviour {
+	[SerializeField] GameObject myChessPrefab;
+	[SyncVar] int myChessCount = 0;
+	int[] numbers = new int[5];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!base.isLocalPlayer)
+			return;
+
+		if (Input.GetKeyDown (KeyCode.Space))
+			Cmd_CreateChess ();
+	}
+
+	[Command]
+	public void Cmd_CreateChess()
+	{
+		// create server-side instance
+
+		GameObject t_chess = (GameObject)Instantiate (myChessPrefab, Random.insideUnitCircle * 4, Quaternion.identity);
+
+		myChessCount++;
+
+		// spawn on the clients
+
+		NetworkServer.Spawn (t_chess);
+	}
+}
