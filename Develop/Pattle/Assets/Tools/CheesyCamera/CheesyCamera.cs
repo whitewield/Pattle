ceb737ed4fc344ef60b52ae6fd35a26c20ef8deb
Hang <whitewield@126.com>
2018-03-29 15:05:35 -0400

    fix iphone x

---
+++ Develop/Pattle/Assets/Tools/CheesyCamera/CheesyCamera.cs
@@ -0,0 +1,66 @@
+﻿//CheesyCamera ver.1.0
+//made by Hang Ruan, Kevin Zeng
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheesyCamera : MonoBehaviour {
+	private enum ResizeType{
+		OrthographicSize,
+		ViewportRect
+	}
+	[SerializeField] ResizeType myType = ResizeType.ViewportRect;
+	private Camera myCamera;
+	[SerializeField] Vector2 myDefaultRatio = new Vector2 (16, 9);
+	private Vector2 myLastScreenSize = Vector2.zero;
+	private float myOrthographicSize;
+
+	// Use this for initialization
+	void Start () {
+		myCamera = this.GetComponent<Camera> ();
+		myOrthographicSize = this.GetComponent<Camera> ().orthographicSize;
+		UpdateResize ();
+		myLastScreenSize = new Vector2 (Screen.width, Screen.height);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Screen.width != myLastScreenSize.x || Screen.height != myLastScreenSize.y) {
+			UpdateResize ();
+			myLastScreenSize = new Vector2 (Screen.width, Screen.height);
+		}
+	}
+
+	private void UpdateResize () {
+		switch (myType) {
+		case ResizeType.OrthographicSize: 
+			UpdateOrthographicSize ();
+			break;
+		case ResizeType.ViewportRect: 
+			UpdateViewportRect ();
+			break;
+		}
+	}
+
+	private void UpdateViewportRect () {
+		if ((float)Screen.height / (float)Screen.width == myDefaultRatio.y / myDefaultRatio.x) {
+			myCamera.rect = new Rect (0, 0, 1, 1);
+		} else if ((float)Screen.height / (float)Screen.width > myDefaultRatio.y / myDefaultRatio.x) {
+			float t_height = myDefaultRatio.y / myDefaultRatio.x / (float)Screen.height * (float)Screen.width;
+			myCamera.rect = new Rect (0, (1 - t_height) * 0.5f, 1, t_height);
+		} else {
+			float t_width = myDefaultRatio.x / myDefaultRatio.y / (float)Screen.width * (float)Screen.height;
+			myCamera.rect = new Rect ((1 - t_width) * 0.5f, 0, t_width, 1);
+		}
+
+
+	}
+
+	private void UpdateOrthographicSize () {
+		if ((float)Screen.height / (float)Screen.width > myDefaultRatio.y / myDefaultRatio.x)
+			myCamera.orthographicSize = myOrthographicSize * myDefaultRatio.x / myDefaultRatio.y / (float)Screen.width * (float)Screen.height;
+		else
+			myCamera.orthographicSize = myOrthographicSize;
+//		myCamera.orthographicSize = myOrthographicSize * myDefaultRatio.y / myDefaultRatio.x / (float)Screen.height * (float)Screen.width;
+	}
+}
---
+++ Develop/Pattle/Assets/Tools/CheesyCamera/CheesyCanvas.cs
@@ -0,0 +1,45 @@
+﻿//CheesyCamera ver.1.0
+//made by Hang Ruan, Kevin Zeng
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheesyCanvas : MonoBehaviour {
+	private CanvasScaler myCanvasScaler;
+
+	private Vector2 myLastScreenSize = Vector2.zero;
+
+	private Vector2 myDefaultSize;
+
+	// Use this for initialization
+	void Start () {
+		myCanvasScaler = this.GetComponent<CanvasScaler> ();
+		myDefaultSize = myCanvasScaler.referenceResolution;
+		UpdateResize ();
+		myLastScreenSize = new Vector2 (Screen.width, Screen.height);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Screen.width != myLastScreenSize.x || Screen.height != myLastScreenSize.y) {
+			UpdateResize ();
+			myLastScreenSize = new Vector2 (Screen.width, Screen.height);
+		}
+	}
+
+	private void UpdateResize () {
+
+		if ((float)Screen.height / (float)Screen.width == myDefaultSize.y / myDefaultSize.x) {
+			myCanvasScaler.referenceResolution = myDefaultSize;
+		} else if ((float)Screen.height / (float)Screen.width > myDefaultSize.y / myDefaultSize.x) {
+			float t_height = (float)Screen.height / (float)Screen.width * myDefaultSize.x;
+			myCanvasScaler.referenceResolution = new Vector2 (myDefaultSize.x, t_height);
+		} else {
+			float t_width = (float)Screen.width / (float)Screen.height * myDefaultSize.y;
+			myCanvasScaler.referenceResolution = new Vector2 (t_width, myDefaultSize.y);
+		}
+
+	}
+
+}
