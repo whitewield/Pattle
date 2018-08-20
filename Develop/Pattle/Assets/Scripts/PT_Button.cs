55ec57e24cb8d6fbd9954d2c6e0a758d230b6088
Hang <whitewield@126.com>
2018-08-20 15:38:32 -0400

    button update

--- Develop/Pattle/Assets/Scripts/PT_Button.cs
+++ Develop/Pattle/Assets/Scripts/PT_Button.cs
@@ -8,13 +8,13 @@ using Hang.AiryAudio;
 
 
 public class PT_Button : MonoBehaviour {
-
+	// for position change
 	[SerializeField] bool doPositionChange = false;
 	[SerializeField] Transform myButtonTransform;
 	[SerializeField] Vector3 myNormalPosition;
 	[SerializeField] Vector3 myPressedPosition;
 
-	//my event
+	// button event
 	[Serializable]
 	public class PT_ButtonEvent : UnityEvent { }
 
@@ -22,32 +22,46 @@ public class PT_Button : MonoBehaviour {
 	private PT_ButtonEvent onClick = new PT_ButtonEvent();
 	public PT_ButtonEvent onClickEvent { get { return onClick; } set { onClick = value; } }
 
+	// check if it's pressed
 	private bool isPressed;
 
+	// sound
 	[SerializeField] AiryAudioData myAiryAudioData_Down;
 
+	/// <summary>
+	/// if the mouse pressed down
+	/// </summary>
 	public void OnMouseDown () {
+		// play sound
 		if (myAiryAudioData_Down != null)
 			myAiryAudioData_Down.Play ();
-
+		// change position
 		if (doPositionChange)
 			myButtonTransform.localPosition = myPressedPosition;
-
+		// set isPressed to true
 		isPressed = true;
 	}
 
+	/// <summary>
+	/// if the mouse back up
+	/// </summary>
 	public void OnMouseUp () {
+		// change position
 		if (doPositionChange)
 			myButtonTransform.localPosition = myNormalPosition;
-
+		// activate the pressing event
 		if (isPressed)
 			onClickEvent.Invoke ();
 	}
 
+	/// <summary>
+	/// if the mouse exit from the button
+	/// </summary>
 	public void OnMouseExit() {
+		// change position
 		if (doPositionChange)
 			myButtonTransform.localPosition = myNormalPosition;
-		
+		// set isPressed to false
 		isPressed = false;
 	}
 
